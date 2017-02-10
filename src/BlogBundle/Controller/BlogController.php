21e605b9ead9ccdb7fd19a3dc0b5357dfb74fa21
bruno-bouvet <bouvet.bruno@gmail.com>
2017-02-10 09:22:32 +0100

    contact functionnalities -not finished-

--- src/BlogBundle/Controller/BlogController.php
+++ src/BlogBundle/Controller/BlogController.php
@@ -3,6 +3,9 @@
 namespace BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use BlogBundle\Entity\Contact;
+use Symfony\Component\BrowserKit\Request;
+use BlogBundle\Form\ContactType;
 
 class BlogController extends Controller
 {
@@ -16,8 +19,27 @@ class BlogController extends Controller
         return $this->render('@Blog/Blog/about.html.twig');
     }
 
-    public function contactAction()
+    public function contactAction(Request $request)
     {
-        return $this->render('@Blog/Blog/contact.html.twig');
+        $contact = new Contact();
+        $form = $this->createForm(new ContactType(), $contact);
+
+        if ($request->getMethod() == 'POST') {
+            $form->handleRequest($request);
+
+            if ($form->isValid()) {
+
+                return $this->redirect($this->generateUrl('blog_contact'));
+            }else{
+//                     An error ocurred, handle
+//                    var_dump("Errooooor :(");
+//                }
+        }
+
+        return $this->render('@Blog/Blog/contact.html.twig', array(
+            'form' => $form->createView()
+        ));
     }
+}
+
 }
---
+++ src/BlogBundle/Form/ContactType.php
@@ -0,0 +1,27 @@
+<?php
+namespace BlogBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
+class ContactType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name');
+        $builder->add('email', 'email');
+        $builder->add('subject');
+        $builder->add('body', 'textarea');
+    }
+
+    public function getName()
+    {
+        return 'contact';
+    }
+}
